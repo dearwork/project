ac495bc98697234b0f6ef4037f2b0a8c59d1d7f1
depark <depark@wylie.co.kr>
2022-12-26 15:35:10 +0900

    first commit

---
+++ index/common/js/status.js
@@ -0,0 +1,34 @@
+
+
+// tab 이동
+function tabLink() {
+	$(".tab_wrap li").click(function() {
+		var tabId = $(this).index();
+
+		$(this).addClass("on");
+		$(this).siblings().removeClass("on");
+
+		$(".menu_section").removeClass("all");
+		$(".menu_section").eq(tabId - 1).addClass("on").siblings(".menu_section").removeClass("on");
+		if($(this).hasClass("all")){
+			$(".menu_section").addClass("all");
+			$(".menu_cont").removeClass("on");
+		}
+	});
+	$(".tab_wrap li:first-child").click();
+}
+
+function moreDetail() {
+	$(".comment .more_detail").click(function() {
+		var strong = $(this),
+			divParent = strong.closest(".more_detail");
+
+		if(divParent.hasClass("open")){
+			divParent.removeClass("open")
+		}else {
+			divParent.addClass("open");
+		}
+	})
+}
+
+// sub_tab 이동
