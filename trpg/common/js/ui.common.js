05d88cad808a8f68bf59b15c4550c574f5ba3629
depark <depark@wylie.co.kr>
2022-12-30 11:08:15 +0900

    2022-12-30 파비콘 추가 및 meta 불러오기 취소

--- trpg/common/js/ui.common.js
+++ trpg/common/js/ui.common.js
@@ -1,11 +1,11 @@
 
 //inc
-$(document).ready (function() {
-	$.ajax({
-		url : "./../inc/head.html",
-		dataType : "html",
-		success : function(results) {
-			$(results).appendTo('head');
-		}
-	});
-});
+// $(document).ready (function() {
+// 	$.ajax({
+// 		url : "./../inc/head.html",
+// 		dataType : "html",
+// 		success : function(results) {
+// 			$(results).appendTo('head');
+// 		}
+// 	});
+// });
